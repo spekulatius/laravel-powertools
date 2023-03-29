deaf36626226f53dc4b4846033e19105ac5448fe
Peter Thaleikis <spekulatius@users.noreply.github.com>
2023-03-29 08:05:23 +0200

    Extracting MaskSensitiveData into Collection makro (#3)
    * Extracting MaskSensitiveData into Collection makro
    
    * Fix styling
    
    * Tweaking comment (code ocd)
    
    * Improve MaskSensitiveData and add PHPStan baseline

--- config/powertools.php
+++ config/powertools.php
@@ -1,6 +1,27 @@
 <?php
 
-// config for Spekulatius/LaravelPowertools
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Masked fields
+    |--------------------------------------------------------------------------
+    |
+    | These properties will be replaced with "[masked]",
+    |   when logged via toLog or the model tracker.
+    |
+    */
+
+    'masked_fields' => [
+        '/password/i',
+        '/passwd/i',
+        '/hash/i',
+        '/checksum/i',
+        '/salt/i',
+        '/token/i',
+        '/key/i',
+        '/secret/i',
+        '/acl/i',
+    ],
 
 ];
--- src/LaravelPowertoolsServiceProvider.php
+++ src/LaravelPowertoolsServiceProvider.php
@@ -2,12 +2,40 @@
 
 namespace Spekulatius\LaravelPowertools;
 
+use Illuminate\Support\Collection;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Spekulatius\LaravelPowertools\Commands\LaravelPowertoolsCommand;
 
 class LaravelPowertoolsServiceProvider extends PackageServiceProvider
 {
+    public function boot(): void
+    {
+        // Mask some sensitive data in a collection
+        Collection::macro('maskSensitiveData', function () {
+            // Prepare the masked fields regexes for prime time.
+            $regexes = (array) config('powertools.masked_fields', []);
+
+            // Apply the mask to the sensitive data based on the regexes and custom fields
+            return $this->map(function ($value, $key) use (&$regexes) {
+                // Early return null - masking this won't make any sense...
+                if ($value === null) {
+                    return null;
+                }
+
+                // Check if the key matches one of the regexes
+                foreach ($regexes as $regex) {
+                    if (preg_match($regex, $key)) {
+                        return '[masked]';
+                    }
+                }
+
+                // Return the original value if it's not sensitive
+                return $value;
+            });
+        });
+    }
+
     public function configurePackage(Package $package): void
     {
         /*
@@ -16,8 +44,8 @@ class LaravelPowertoolsServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('laravel-powertools');
-        // ->hasConfigFile()
+            ->name('laravel-powertools')
+            ->hasConfigFile();
         // ->hasViews()
         // ->hasMigration('create_laravel-powertools_table')
         // ->hasCommand(LaravelPowertoolsCommand::class);
